fb92d96e2f87296dbf3316b7a6eb76ebf1f7cd9b
planny <plan9139@gmail.com>
2021-09-09 11:51:51 +0700

    hw2

--- Assets/Scripts/MyScript.cs
+++ Assets/Scripts/MyScript.cs
@@ -4,37 +4,14 @@ using UnityEngine;
 
 public class MyScript:MonoBehaviour
 {
-    int n = 0;
-
     void Start()
     {
-        
-		for (int n = 1; n <= 100; n++)	
-       {
-         print(n);
-       }
+      int z = 1;
+      while( z <= 100)
+      print(z);
+      z = z + 2;
 
-    for (int n=1; n <= 100; n++) 
-      {
-      if(n%2==0)
-        {
-          print(n);
-        }
-    
-      } 
-      float x = 1.0f;
-      float r = circle(x);
-
-    float circle(float r)
-      {
-        float result;
-        r = 2.0f;
-        result = 3.14f * (r*r);
-        print (result);
-        return result;
-      } 
-
-    }  
-    
+    }
+   
 }
 
